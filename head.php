5f408b92a0f2e0dfa28130b127784b4558aedcb9
MyAudom <149916241+MyAudom@users.noreply.github.com>
2025-09-17 10:38:50 +0700

    Update head.php

--- head.php
+++ head.php
@@ -1,9 +1,9 @@
 <?php
 session_start();
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "admin_db";
+$servername = "db.pxxl.pro:25543";
+$username = "user_f4df9832";
+$password = "d540fc259c55d102bd5f787000a82d85";
+$dbname = "db_59a146df";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
@@ -12,4 +12,5 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
+
 ?>
